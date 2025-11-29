871909626af78a26d38f08afe78ba815679b5b6d
augustanol <266454@student.pwr.edu.pl>
2025-11-29 17:57:14 +0100

    rcon test

--- app/api/endpoints/rcon.py
+++ app/api/endpoints/rcon.py
@@ -3,6 +3,7 @@ from app.services.rcon_connection import rcon_command
 
 router = APIRouter()
 
+
 @router.get("/rcon")
 async def server_status():
     responses = []
@@ -10,5 +11,5 @@ async def server_status():
     responses.append(await rcon_command("currentEvent"))
     responses.append(await rcon_command("eventsList"))
     responses.append(await rcon_command("players"))
-    responses.append(await rcon_command("serverStats"))
+    responses.append(await rcon_command("findPlayer 76561198271197381"))
     return responses
