5696da7ed97e0035206463cb91ad71447ebcdfb3
augustanol <266454@student.pwr.edu.pl>
2025-12-18 16:40:07 +0100

    alembix fixes

--- app/db/models/player.py
+++ app/db/models/player.py
@@ -1,8 +1,6 @@
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column
 from sqlalchemy import String, Integer
-
-class Base(DeclarativeBase):
-    pass
+from app.db.models.base import Base
 
 
 class Player(Base):
@@ -10,4 +8,6 @@ class Player(Base):
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     name: Mapped[str] = mapped_column(String(100), nullable=False)
-    steam_id: Mapped[str] = mapped_column(String(32), unique=True, index=True, nullable=False)
+    steam_id: Mapped[str] = mapped_column(
+        String(32), unique=True, index=True, nullable=False
+    )
